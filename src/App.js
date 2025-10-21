60720e159767118fa51a57f644acb8c20beb4b72
Shri Harini Durairaj <shriharinidurairaj@gmail.com>
2025-10-21 23:11:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+  const handleSubmit = async () => {
+    setFullName(`${firstName} ${lastName}`);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      First Name
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      Last Name
+      <button onClick={handleSubmit}>Submit</button>
+      {fullName && <h2>Full Name: {fullName}</h2>}
     </div>
   );
 }
